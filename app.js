ab7d52f881d5a1409cbec0cbd8fd8c8785c105ca
ashwaniyadav <selnoxreact@gmail.com>
2024-12-13 14:27:28 +0530

    gfh

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const userRouter = require('./Routes/User');
 const AuthRouter = require('./Routes/Auth');
 const { mongodbconnect } = require('./Views/Connection');
 const cors = require('cors');
 
+const mongoURI = process.env.MONGO_URI;
 
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
 
 // const upload = multer({ dest: "uploads/" });
 app.use(cors({ origin: '*' }));
@@ -15,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/user', userRouter);
 app.use('/auth', AuthRouter);
 
-mongodbconnect();
+// mongodbconnect();
 app.get('/', async (req, res) => {
     res.send("You are in the home page");
 });
